e7eeb1d017eda6f946d4f0d9325363c628d2b312
Paul Barnabas <bizpally@gmail.com>
2016-04-14 18:30:26 +0100

    Using jquery to delete items by clicking on them

--- script.js
+++ script.js
@@ -3,4 +3,7 @@ $(document).ready(function () {
       var addVar = $("input[name=checkListItem]").val();
       $('.list').append('<div class="item">' + addVar + '</div>');
     });
- });
+    $(document).on('click', '.item', function() {
+      $(this).remove();
+    });
+});
